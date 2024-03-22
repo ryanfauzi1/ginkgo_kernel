65abe0331422eea50bd3b1715b2d4177701f2f16
Ryan Fauzi <76712935+ryanfauzi1@users.noreply.github.com>
2024-03-22 09:51:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ KERNEL_DEFCONFIG=fury-perf_defconfig ## Ini defconfignya setiap type hape beda2 
 ANYKERNEL3_DIR=$PWD/AnyKernel3/ ## ini anykernel nya gunanya untuk membukus hasil compile untuk siap flash
 ZIPNAME=fury-pinjamvps-ksu-$(date '+%Y%m%d').zip ## INI NAMA KERNEL zip NYA
 
-if ! [ -d "$HOME/proton" ]; then
-echo "proton clang not found! Cloning..."
-if ! git clone -q https://github.com/kdrag0n/proton-clang.git --depth=1 -b master ~/proton; then ## ini Clang nya tools untuk membangun/compile kernel nya (tidak semua kernel mendukung clang)
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
-chmod +x /root/proton-clang/bin/clang
-export PATH="/root/proton-clang/bin:$PATH"
+#if ! [ -d "$HOME/proton" ]; then
+#echo "proton clang not found! Cloning..."
+#if ! git clone -q https://github.com/kdrag0n/proton-clang.git --depth=1 -b master ~/proton; then ## ini Clang nya tools untuk membangun/compile kernel nya (tidak semua kernel mendukung clang)
+#echo "Cloning failed! Aborting..."
+#exit 1
+#fi
+#fi
+
+#export PATH="/root/proton-clang/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="$(/root/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+#export KBUILD_COMPILER_STRING="$(/root/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
 # Speed up build process
 MAKE="./makeparallel"
